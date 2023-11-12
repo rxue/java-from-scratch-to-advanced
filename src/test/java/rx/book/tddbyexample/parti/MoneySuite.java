eb8c5d4ed9bd546b7991f0c4d319159dc12bf3da
rui <ruixue.fi@gmail.com>
2023-11-12 22:06:33 +0200

    #5 MoneySuite added to run tests in TDDByExample PartI tests

---
+++ src/test/java/rx/book/tddbyexample/parti/MoneySuite.java
@@ -0,0 +1,10 @@
+package rx.book.tddbyexample.parti;
+
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("rx.book.tddbyexample.parti")
+class MoneySuite {
+}
