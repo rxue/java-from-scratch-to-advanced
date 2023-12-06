d467890b41f9701d07dab815c1e9d82540a51f36
rui <ruixue.fi@gmail.com>
2023-12-06 23:22:17 +0200

    #7 test on environment variable

---
+++ src/test/java/rx/it/EnvironmentVariableIT.java
@@ -0,0 +1,12 @@
+package rx.it;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EnvironmentVariableIT {
+    @Test
+    public void testEnvrionmentVariable() {
+        assertEquals("test", System.getenv("y"));
+    }
+}
