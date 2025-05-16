c664a1699f57e76bce53aabe32f34d7fa02d3138
Rui Xue <rui.xue@fasolutions.com>
2025-05-16 17:55:30 +0300

    Hibernate JPA in Action book

--- jpa-hibernate/src/test/java/rx/book/javapersistencewithhibernate1revised/ch9/MergeItemIT.java
+++ jpa-hibernate/src/test/java/rx/book/javapersistencewithhibernate1revised/ch9/MergeItemIT.java
@@ -20,21 +20,24 @@ public class MergeItemIT extends AbstractITConfigTemplate {
             em.persist(item);
             transaction.commit();
         }
-        //
-        item.setDescription("update");// Detached entity instance
-        //
+        //ACT
+        item.setDescription("updated");// Detached entity instance
+        Item returnedMergedItem;
         try (EntityManager em = entityManagerFactory.createEntityManager()) {
             EntityTransaction transaction = em.getTransaction();
             transaction.begin();
-            Item mergedItem = em.merge(item);
+            returnedMergedItem = em.merge(item);
             transaction.commit();
-            assertFalse(mergedItem == item);
-            assertEquals(item.getId(), mergedItem.getId());
         }
+        //ASSERT
+        assertFalse(returnedMergedItem == item, "merge method should return a new instance of the given entity");
+        assertEquals(item.getId(), returnedMergedItem.getId());
+        assertEquals("updated", returnedMergedItem.getDescription());
     }
     @Test
     public void merge_newItemWithoutId() {
         Item item;
+        Item returnedMergedItem;
         try (EntityManager em = entityManagerFactory.createEntityManager()) {
             EntityTransaction transaction = em.getTransaction();
             transaction.begin();
@@ -42,29 +45,35 @@ public class MergeItemIT extends AbstractITConfigTemplate {
 
             item.setDescription("original desc");
             assertNull(item.getId());
-            Item mergedItem = em.merge(item);
+            returnedMergedItem = em.merge(item);
             transaction.commit();
-            assertNotNull(mergedItem.getId());
-            assertNotNull(item.getId());
-            assertNotEquals(item, mergedItem);
         }
+        //ASSERT
+        Long returnedId = returnedMergedItem.getId();
+        assertNotNull(returnedId);
+        assertTrue(returnedId == item.getId());
+        assertNotEquals(item, returnedMergedItem);
     }
 
     @Test
     public void merge_newItemWithId() {
-        Item item;
+        Item item = new Item();
+        final Long selfDefinedId = 1000L;
+        item.setId(selfDefinedId);
+        item.setDescription("original desc");
+        Item returnMergedItem;
+        //ACT
         try (EntityManager em = entityManagerFactory.createEntityManager()) {
             EntityTransaction transaction = em.getTransaction();
             transaction.begin();
-            item = new Item();
-            item.setId(1000L);
-            item.setDescription("original desc");
-            Item mergedItem = em.merge(item);
+            returnMergedItem = em.merge(item);
             transaction.commit();
-            assertNotEquals(1000L, item.getId());
-            assertTrue(mergedItem.getId() == item.getId());
-            assertNotEquals(item, mergedItem);
+
         }
+        //ASSERT
+        assertNotEquals(selfDefinedId, item.getId(), "predefined ID is overridden by EntityManager during merge");
+        assertTrue(returnMergedItem.getId() == item.getId());
+        assertNotEquals(item, returnMergedItem);
     }
 
 }
